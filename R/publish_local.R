84d43d04ef0e9d6b463df8abcd8e2080abb99427
Josh Christensen <joshuarc13@gmail.com>
2023-11-28 18:16:47 -0700

    Clarify documentation and move the publish_local function closer to functioning.

--- R/publish_local.R
+++ R/publish_local.R
@@ -5,7 +5,8 @@
 #' Create or update a system of subdirectories that is recognized by `utils::install.packages` as a
 #' package repository. The function publishes the referenced package both as a source tarball and as
 #' a binary package under the current operating system. As a result, installation of the package
-#' will fail if attempted from a different operating system with no available toolchain.
+#' will fail if attempted from a different operating system with no available toolchain unless
+#' a separate binary is created and added.
 #'
 #' @param repository_path Folder in which to create the repository or update an existing one.
 #' @param package_path Path to the parent directory of the package to be published.
@@ -21,9 +22,12 @@ publish_local <- function(
     repository_path,
     package_path = "."
     ) {
+  # Extract the current version of R
+  r_version <- paste0(R.version$major, ".", substr(R.version$minor, 1, 1))
   # Determine where the tarball should be located
-  tarball_location <- base::file.path(repository_path, "src", "contrib")
-  # Determine where the binary package belongs
-  os <- base::Sys.info()['sysname']
-
+  tarball_location <- file.path(repository_path, "src", "contrib")
+  # Determine where the windows binary should be located
+  windows_bin_location <- file.path(repository_path, "bin", "windows", "contrib", r_version)
+  # Build the tarball
+  pkgbuild::build(pkg = package_path)
 }
